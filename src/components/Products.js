bef7ff463abac7afbab5171abfc5f406bc8de072
Alex Battle <58659536+alex-battle@users.noreply.github.com>
2020-04-14 09:28:41 -0400

    update, everything works

--- src/components/Products.js
+++ src/components/Products.js
@@ -8,7 +8,7 @@ class Products extends React.Component {
             <div className="col-md-4" key={product.id}>
                 <div className="thumbnail text-center">
                     <a href={`#${product.id}`} onClick={this.props.handleAddToCart}>
-                        <img src={`/products/${product.sku}_1.jpg`} alt={product.title} />
+                        <img src={`/products/${product.sku}_1.jpg/`} alt={product.title} />
                         <p>
                             {product.title}
                         </p>
