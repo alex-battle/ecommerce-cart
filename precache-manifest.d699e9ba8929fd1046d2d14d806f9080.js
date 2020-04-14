3b184e39b680874b2981648ecc76fcdce4846dbf
Alex Battle <58659536+alex-battle@users.noreply.github.com>
2020-04-14 07:00:38 -0400

    Updates

---
+++ precache-manifest.d699e9ba8929fd1046d2d14d806f9080.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4b30b5bb50f24fbdb781d788f04f4e4",
+    "url": "/ecommerce-shopping/index.html"
+  },
+  {
+    "revision": "7414852011c3d8e315e4",
+    "url": "/ecommerce-shopping/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a92236243bd032f1af52",
+    "url": "/ecommerce-shopping/static/js/2.099fd57f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ecommerce-shopping/static/js/2.099fd57f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7414852011c3d8e315e4",
+    "url": "/ecommerce-shopping/static/js/main.74826a93.chunk.js"
+  },
+  {
+    "revision": "d81128fddc12754b9ae1",
+    "url": "/ecommerce-shopping/static/js/runtime-main.2160e4ae.js"
+  }
+]);
